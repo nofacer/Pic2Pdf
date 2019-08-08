e4dad34fa0d5dc3718471c126af8a91830771aca
Xubin Ni <xubinni@XubindeMacBook-Pro.local>
2019-08-08 15:52:31 +0800

    add babel to transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    '/node_modules/'
+    '/node_modules/(?!@babel)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
